c5c696212dfc4d343fc712ef2bb1d30b28fd9631
ShubhamGadhiya1012 <gadhiyashubham10@gmail.com>
2026-06-10 18:11:51 +0530

    channe  in Site transfer , issue report

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -2,15 +2,15 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 class ApiService {
-  // static const String kBaseUrl =
-  //     'http://192.168.0.145:5209/api'; // Dhruvilbhai Debugging
+  static const String kBaseUrl =
+      'http://192.168.0.145:5209/api'; // Dhruvilbhai Debugging
   // static const String kBaseUrl =
   //     'http://192.168.0.145:5020/api'; // Dhruvilbhai Local
   // static const String kBaseUrl = 'http://160.187.80.215:8080/api'; // Live
 
   // static const String kBaseUrl = 'http://192.168.0.145:5020/api'; // Backup URL
 
-  static const String kBaseUrl = 'http://shivayapp.jineecs.in/api'; // Plesk URL
+  // static const String kBaseUrl = 'http://shivayapp.jineecs.in/api'; // Plesk URL
 
   static Future<dynamic> getRequest({
     String? endpoint,
